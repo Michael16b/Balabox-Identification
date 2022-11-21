b829ee7e0f9a41c796aa10cefea27a5524f3be4a
Eve-Anne Offredi <offredi.e2100609@etud-univ.ubs.fr>
2022-11-21 19:17:26 +0100

    php extensions

--- Identification/views/main.php
+++ Identification/views/main.php
@@ -1,5 +1,8 @@
 <?php include __ROOT__."/views/header.html"; ?>
 <h1> Main page</h1>
 <a href="/connect">Click here to display the connection form.</a>
+<?php
+phpinfo();
+?>
             
 <?php include __ROOT__."/views/footer.html"; ?>
