69324110306bcbccae306afe4d433f67cb26c3ee
Evahhhh <offredi.e2100609@etud-univ.ubs.fr>
2022-11-29 09:55:05 +0100

    update pure css

--- Identification/views/main.php
+++ Identification/views/main.php
@@ -3,6 +3,18 @@ include __ROOT__."/views/header.html";
 ?>
 
 <body>
+    <form class="pure-form pure-form-stacked">
+        <fieldset>
+            <input type="text" id="stacked-num" placeholder="Numéro associé" />
+            <input type="password" id="stacked-password" placeholder="Mot de passe" />
+
+            <button type="submit" class="pure-button pure-button-primary">Sign in</button>
+        </fieldset>
+    </form>
+</body>
+
+
+<!-- <body>
     <div class="ui segment custom-img">
         <img class="ui centered medium image" src="../static/assets/logo_balabox.png">
     </div>
@@ -20,4 +32,4 @@ include __ROOT__."/views/header.html";
     </form>
 
     <a href="/seeConfig">Click here to see your php config.</a>
-</body>
+</body> -->
