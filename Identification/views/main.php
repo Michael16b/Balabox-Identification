074be4cfb3398e6d5712e26631ab2f5a65ab3aaf
Evahhhh <offredi.e2100609@etud-univ.ubs.fr>
2022-11-30 09:50:05 +0100

    update view connection page

--- Identification/views/main.php
+++ Identification/views/main.php
@@ -2,38 +2,17 @@
 include __ROOT__."/views/header.html";
 ?>
 
+
 <body>
     <div class="center-container">
-        <img class="pure-img" src="../static/assets/logo_balabox.png">
+        <img class="pure-img logo-acc" src="../static/assets/logo_balabox.png">
         <form class="pure-form pure-form-stacked">
             <fieldset>
-                <input type="text" id="stacked-num" placeholder="Numéro associé" />
-                <input type="password" id="stacked-password" placeholder="Mot de passe" />
-    
-                <button type="submit" class="pure-button pure-button-primary">Se connecter</button>
+                <input type="text" id="stacked-num" placeholder="Numéro associé" /></label><br>
+                <input type="password" id="stacked-password" placeholder="Mot de passe" /><br>
+                <button type="submit" class="pure-button pure-button-primary">Connexion</button>
             </fieldset>
             <button type="submit" class="pure-button pure-button-txt">Je suis professeur</button>
         </form>
     </div>
 </body>
-
-
-<!-- <body>
-    <div class="ui segment custom-img">
-        <img class="ui centered medium image" src="../static/assets/logo_balabox.png">
-    </div>
-
-    <form class="ui form" action="/connect" method="post">
-        <div class="field">
-            <label>Identifiant</label>
-            <input type="text" name="id" placeholder="Ton identifiant">
-        </div>
-        <div class="field">
-            <label>Mot de passe</label>
-            <input type="password">
-        </div>
-        <button class="ui orange button" type="submit">Connexion</button>
-    </form>
-
-    <a href="/seeConfig">Click here to see your php config.</a>
-</body> -->
