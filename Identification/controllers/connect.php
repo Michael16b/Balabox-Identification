a5735af6312f442807f300d73e95c57ebc6c1f1e
Evahhhh <offredi.e2100609@etud-univ.ubs.fr>
2022-12-07 09:07:37 +0100

    remove useless files

--- Identification/controllers/connect.php
+++ Identification/controllers/connect.php
@@ -3,10 +3,6 @@ require(__ROOT__.'/controllers/Controller.php');
 
 class ConnectController extends Controller{
 
-    public function get($request){
-        $this->render('main',[]);
-    }
-
     public function post($request){
         $this->render('connect_info',['num' => $request['num'], 'password' => $request['password'], 'idprof' => null]);
     }
