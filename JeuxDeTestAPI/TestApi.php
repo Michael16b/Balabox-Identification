b5ab0b90f43f0fa484bf53c2f68ed3603b935a11
Thomas Poulain <95443024+Thomas-Poulain@users.noreply.github.com>
2022-12-12 11:40:34 +0100

    modif test api

--- JeuxDeTestAPI/TestApi.php
+++ JeuxDeTestAPI/TestApi.php
@@ -21,11 +21,17 @@
             var_dump($user);
             echo "<br>";
 
+            //test2 récupérer info par suername et lastname
+            $user = $BD->get_record('user', ['firstname' => 'G1', 'password' => 'G1']);
+            echo "récupérer info user firstname = G1 password = G1 <br>";
+            var_dump($user);
+            echo "<br>";
+
             //test 3 insérer de la données par fisrtname et lastname
             $userTest = new stdClass();
             $userTest->firstname = 'test';
             $userTest->lastname = 'test';
-            $idTest = $DB->insert_record('user', $userTest);
+            $idTest = $CFG->user_create_user($userTest, false, false);
             echo "insérer user fistname = test lastaname = test<br>";
 
             //test 4 insérer de la données par fisrtname et lastname et paswword
@@ -33,27 +39,33 @@
             $userTest1->firstname = 'test1';
             $userTest1->lastname = 'test1';
             $userTest1->password = 'test1';
-            $idTest1 = $DB->insert_record('user', $userTest1);
+            $idTest1 = $CFG->user_create_user($userTest1, true, false);
             echo "ajouter un user firstname = test1 lastname = test1 password = test1 <br>";
 
             //test 5 insérer de la données par fisrtname et lastname et paswword
             $userTest2 = new stdClass();
-            $userTest2->firstname = 'test1';
-            $userTest2->lastname = 'test1';
-            $userTest2->password = 'test1';
-            $idTest2 = $DB->insert_record('user', $userTest1);
-            $idTest2 = $DB->insert_record('user', ['firstname' => 'test1', 'lastname' => 'test1', 'password' => 'test1']);
+            $userTest2->firstname = 'test2';
+            $userTest2->lastname = 'test2';
+            $userTest2->password = 'test2';
+            $userTest2->
+            $idTest1 = $CFG->user_create_user($userTest1, true, false);
             echo "ajouter un user firstname = test2 lastname = test2 password = test2 <br>";
 
-            //test 4 récupérer information du nouveau user
+            //test 6 récupérer information du nouveau user test
             $user = $BD->get_record('user', ['firstname' => 'test', 'lastname' => 'test', 'password' => 'test']);
             echo "récupérer info user test test <br>";
             var_dump($user);
             echo "<br>";
 
-            //test 7 récupérer information du nouveau user
+            //test 7 récupérer information du nouveau user test1
             $user = $BD->get_record('user', ['firstname' => 'test1', 'lastname' => 'test1', 'password' => 'test1']);
-            echo "récupérer info user test1 test1 <br>";
+            echo "récupérer info user test1 test1 test2<br>";
+            var_dump($user);
+            echo "<br>";
+
+            //test 7 récupérer information du nouveau user test2
+            $user = $BD->get_record('user', ['firstname' => 'test2', 'lastname' => 'test2', 'password' => 'test2']);
+            echo "récupérer info user test2 test2 test2 <br>";
             var_dump($user);
             echo "<br>";
         }
