355f6ed047c782a8fa1d296646df45d2ac93a6c8
Michaël <michael.besily@gmail.com>
2022-12-12 14:14:05 +0100

    Add autoDownload console of Balabox Manager

--- BalaBoxManager/donwloadFile.py
+++ BalaBoxManager/donwloadFile.py
@@ -4,7 +4,7 @@ from tqdm.auto import tqdm
 
 
 
-def downloadFile(url, filename):
+def download(url, filename):
     with requests.get(url, stream=True) as r:
         total_length = int(r.headers.get("Content-Length"))
         with tqdm.wrapattr(r.raw, "read", total=total_length, desc="")as raw:
@@ -14,6 +14,7 @@ def downloadFile(url, filename):
 
 
 
-downloadFile("http://misc.finxol.io/moodlebox.gz", "moodlebox.gz")
+#download("http://misc.finxol.io/moodlebox.gz", "moodlebox.gz")
+#download("http://terminale-colbert-lorient.fr/download/8052/?tmstv=1669851024", "Moodlebox.ova")
 
 
