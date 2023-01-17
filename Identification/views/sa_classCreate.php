8b08fc4a8cb94f8e6b609e0a6b580893455fc00a
Evahhhh <offredi.e2100609@etud-univ.ubs.fr>
2023-01-17 22:17:48 +0100

    update super admin

---
+++ Identification/views/sa_classCreate.php
@@ -0,0 +1,31 @@
+<?php
+include __ROOT__."/views/header.html";
+?>
+
+
+<body>
+    <!-- HEADER -->
+        <nav class="navbar navbar-expand-lg border-bottom">
+            <a class="navbar-brand" href="#">
+                <div class="container-fluid">
+                    <img src="../static/img/logo_balabox.png" alt="Logo"  height="30" class="d-inline-block align-text-top">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Créer une classe</a>
+                        </li>
+                        <li class="nav-item">
+                             <a class="nav-link active" href="#">Créer un utilisateur</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+         </nav>
+
+
+
+</body>
