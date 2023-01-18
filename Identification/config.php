dd5d3f5bcb972fd25a6828e5618b739834d8cc0b
Tpoulain <e2202656@etud.univ-ubs.fr>
2023-01-18 18:32:04 +0100

    remplace

--- Identification/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-//const DB_FILE = __ROOT__.'TO DO';
-const VIEWS_DIR = __ROOT__.'/views';
-const CONTROLLERS_DIR = __ROOT__.'/controllers';
-?>
--- Identification/views/seeconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-include __ROOT__."/views/header.html";
-?>
-
-<h1> Voici votre configuration php :</h1>
-<?php
-phpinfo();
-?>
