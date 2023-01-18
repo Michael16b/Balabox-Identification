d8664b4606a824f7834c78a11911d061178fcc9b
Tpoulain <e2202656@etud.univ-ubs.fr>
2023-01-18 09:20:06 +0100

    adaptation

--- Identification/controllers/connect.php
+++ Identification/controllers/connect.php
@@ -1,10 +1,26 @@
 <?php
+
 require(__ROOT__.'/controllers/Controller.php');
+require(__ROOT__.'/config.php');
+
 
 class ConnectController extends Controller{
+    public function get($request){
+        $this-> render('connect_form', []);
+    }
 
     public function post($request){
-        $this->render('connect_info',['num' => $request['num'], 'password' => $request['password'], 'idprof' => null]);
+        try{
+	  $password = hash('sha256', $password);
+          global $DB;
+          //$user = $DB->get_record('user', array('surname' => $request['surname'], 'password' => $password));
+          $user = $DB-> get_record_sql('SELECT id FROM {USER} WHERE surname = ? AND password = ?', [$request['surname'], $password]);
+            //var_dump($user);
+        }catch (Error $e){
+            echo $e;
+	    $this-> render('connect_info',['surname' => 'Error', 'password' => 'Error', 'idprof' => null]);
+        }
+         $this->render('connect_info',['surname' => $user['surname'], 'password' => $user['password'], 'idprof' => null]);
     }
 }
 
