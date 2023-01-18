fd238f4157f9cefa7cb2d3e62d1a67a8f43dea52
Tpoulain <e2202656@etud.univ-ubs.fr>
2023-01-18 18:22:44 +0100

    update

---
+++ Identification/index2.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+define ("__ROOT__",__DIR__);
+
+// Configuration
+require_once (__ROOT__.'/config2.php');
+
+// ApplicationController
+require_once (CONTROLLERS_DIR.'/ApplicationController.php');
+
+
+// Add routes here
+ApplicationController::getInstance()->addRoute('seeConfig', CONTROLLERS_DIR.'/seeConfig.php');
+ApplicationController::getInstance()->addRoute('connect_Prof', CONTROLLERS_DIR.'/connect_Prof.php');
+ApplicationController::getInstance()->addRoute('connect', CONTROLLERS_DIR.'/connect.php');
+ApplicationController::getInstance()->addRoute('sa_classCreate', CONTROLLERS_DIR.'/sa_classCreate.php');
+ApplicationController::getInstance()->addRoute('sa_userCreate', CONTROLLERS_DIR.'/sa_userCreate.php');
+
+// Process the request
+ApplicationController::getInstance()->process();
+
+?>
