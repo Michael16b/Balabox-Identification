2cbbfebbd8613689f02a647c61c22bdb2b691927
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2023-01-23 00:37:23 +0100

    fix create user

--- Identification/model/userDB.php
+++ Identification/model/userDB.php
@@ -7,18 +7,14 @@ class UserDB {
     }
 
 
-    public final function RandomPassword() : String {
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=';
-        $pass = array();
-        $alphaLength = strlen($alphabet) - 1;
-        for ($i = 0; $i < 8; $i++) {
-            $n = rand(0, $alphaLength);
-            $pass[] = $alphabet[$n];
-        }
-        $pass[rand(0,7)] = $alphabet[rand(0,15)]; // on ajoute un caractère spécial au hasard 
-        $pass[rand(0,7)] = $alphabet[rand(26,35)]; // on ajoute un chiffre au hasard 
-        $pass[rand(0,7)] = $alphabet[rand(52,61)]; // on ajoute une lettre majuscule au hasard 
-        return implode($pass);
+    public final function RandomPassword() {
+        $uppercase = range('A', 'Z');
+        $lowercase = range('a', 'z');
+        $numbers = range(0, 9);
+        $specialChars = array('!', '@', '#', '$', '%', '^', '&', '*');
+        $characters = array_merge($uppercase, $lowercase, $numbers, $specialChars);
+        shuffle($characters);
+        return implode(array_slice($characters, 0, 12));
     }
 
     public function checkUserName(String $username) : String {
@@ -42,6 +38,8 @@ class UserDB {
         $user->lastname = $lastName;
         $user->username = $this->checkUserName(strtolower(substr($firstName,0,1) . $lastName));
         $user->password = $this->RandomPassword();
+        $password = $user->password;
+        // password_hash($password, PASSWORD_DEFAULT);
         $user->email = $firstName."." . $lastName . "@balabox.home" ;
         $user->auth = 'manual';
         $user->confirmed = 1;
@@ -51,7 +49,7 @@ class UserDB {
 
         $user->id = user_create_user($user);
 
-        return json_encode($user);
+        return ($user->username . " " . $password);
         }
 
 
