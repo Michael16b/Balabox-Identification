5d335bf06c524c3b1d35a34d06ffe45f683cc707
Michaël <michael.besily@gmail.com>
2023-02-02 00:16:33 +0100

    modify testUpdateGroups and testAddMember methods

--- Identification/test/groupsDBTest.php
+++ Identification/test/groupsDBTest.php
@@ -6,6 +6,7 @@ class groupsDBTest extends Assertions
     private $groupsDB;
     private $testGroupName = "Test Group";
     private $testDescription = "Test Description";
+    private $testUser = "testuser";
 
 
     public function setUp(): void
@@ -44,25 +45,24 @@ class groupsDBTest extends Assertions
 
     public function testUpdateGroups()
     {
-        $groupName = "Test Group";
-        $desc = "Test Description";
+        $this->setUp();
         $newDesc = "New Test Description";
-        $this->groupsDB->addGroups($groupName, $desc);
-        $this->groupsDB->updateGroups($groupName, $newDesc);
+        $this->groupsDB->addGroups($this->testGroupName, $this->testDescription);
+        $this->groupsDB->updateGroups($this->testGroupName, $newDesc);
 
-        $group = $this->groupsDB->getGroups($groupName);
+        $group = $this->groupsDB->getGroups($this->testGroupName);
         $this->assertEquals($newDesc, $group->description);
+        $this->tearDown();
     }
 
     public function testAddMember()
     {
-        $groupName = "Test Group";
-        $username = "testuser";
-        $this->groupsDB->addGroups($groupName, "Test Description");
-        $this->groupsDB->addMember($groupName, $username);
+        $this->setUp();
+        $this->groupsDB->addGroups($this->testGroupName, $this->testDescription);
+        $this->groupsDB->addMember($this->testGroupName, $this->testUser);
 
-        $members = $this->groupsDB->getMembers($groupName);
-        $this->assertContains($username, $members);
+        $members = $this->groupsDB->getMembers($this->testGroupName);
+        $this->assertContains($this->testUser, $members);
     }
 
     public function testDeleteMember()
