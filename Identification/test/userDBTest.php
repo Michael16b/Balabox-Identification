b66086fd7ce1e51969d5835b338c36b049ce57e7
Michaël <michael.besily@gmail.com>
2023-02-02 00:30:12 +0100

    add view on console of userDBTest

--- Identification/test/userDBTest.php
+++ Identification/test/userDBTest.php
@@ -22,6 +22,7 @@ class userDBTest extends Assertions
 
     public function testGetRecord()
     {   
+        var_dump("testGetRecord");
         $this->setUp();
         // Test the getRecord method
         $surname = array('lastname' => $this->testLastName);
@@ -29,20 +30,24 @@ class userDBTest extends Assertions
         $this->assertNotNull($record);
         $this->assertEquals($this->testUsername, $record->username);
         $this->tearDown();
+        var_dump("testGetRecord : OK");
     }
 
     public function testRandomPassword()
     {   
+        var_dump("testRandomPassword");
         $this->setUp();
         // Test the RandomPassword method
         $password = $this->userdb->RandomPassword();
         $this->assertNotEmpty($password);
         $this->assertEquals(8, strlen($password));
         $this->tearDown();
+        var_dump("testRandomPassword : OK");
     }
 
     public function testAddUser()
     {
+        var_dump("testAddUser");
         $this->setUp();
         // Test the addUser method
         $firstName = 'Jane';
@@ -54,16 +59,19 @@ class userDBTest extends Assertions
         // Clean up by deleting the test user
         $this->userdb->deleteUser($username);
         $this->tearDown();
+        var_dump("testAddUser : OK");
     }
 
     public function testDeleteUser()
     {
+        var_dump("testDeleteUser");
         $this->setUp();
         // Test the deleteUser method
         $this->userdb->deleteUser($this->testUsername);
         $user = $this->userdb->getUser($this->testUsername);
         $this->assertFalse($user);
         $this->tearDown();
+        var_dump("testDeleteUser : OK");
     }
 }
 ?>
