ecc176961b27d28edcc735d6fed6a35122a78aea
Thomas-Poulain <e2202656@etud.univ-ubs.fr>
2023-03-06 20:46:52 +0100

    modification

--- Identification/controllers/sa_userCreate.php
+++ Identification/controllers/sa_userCreate.php
@@ -6,7 +6,7 @@ class SaUserCreateController extends Controller{
 
     public function get($request){
         session_start();
-        if($_SESSION['idRole'] != 1){
+        if($_SESSION['role'] != 1){
             $this->render('sa_error',['message' => "Vous n'avez pas de permission d'entrer dans cette page"]);
         }else{
             $this->render('sa_userCreate',[]);
