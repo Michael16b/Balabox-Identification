d54938b4a59f8776ce9b9bb4c041cdf536099094
Michaël Besily <michael.besily@gmail.com>
2023-03-16 22:49:32 +0000

    Update index2.php

--- Identification/index2.php
+++ Identification/index2.php
@@ -17,7 +17,7 @@ require (__ROOT__.'/model/tokenGenerator.php');
 require (__ROOT__.'/model/usersConnected.php');
 require_once(__ROOT__.'/user/lib.php');
 require_once (__ROOT__.'/config2.php');
-require_once (__ROOT__.'/config-dist.php');
+require_once (__ROOT__.'/config.php');
 // ApplicationController
 require_once (CONTROLLERS_DIR.'/ApplicationController.php');
 
