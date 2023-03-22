e594bc9a1162a3a41f05ad7fbe697fe55079b2a8
Michaël Besily <michael.besily@gmail.com>
2023-03-22 19:36:09 +0000

    add UsersConnected

--- Identification/controllers/main.php
+++ Identification/controllers/main.php
@@ -4,6 +4,14 @@ require(__ROOT__.'/controllers/Controller.php');
 class MainController extends Controller{
 
     public function get($request){
+        if(isset($_SESSION['username'])){
+            $connect = new UsersConnected();
+            $uc->newConnection($request['username'], $role);
+            $this->connection($userRole);
+            $connect->connection($_SESSION['role']);
+        }else{
+            $this->render('connect',[]);
+        }
         $this->render('main',[]);
     }
 }
