624604b2fead36f90ea44cdd289843983e53c657
Michaël <michael.besily@gmail.com>
2023-03-29 19:53:22 +0200

    modification groupsDB

--- Identification/model/groupsDB.php
+++ Identification/model/groupsDB.php
@@ -3,13 +3,11 @@
 class GroupsDB {
 
 
-    public function addGroups(String $groupeName, String $desc, int $courseid) : int{
-        $groupdata = new stdClass();
-        $groupdata->name = $groupeName;
-        $groupdata->courseid = $courseid;
-        $groupdata->description = $desc;
-
-        return groups_create_group($courseid, $groupdata);
+    public function addGroups(String $groupeName, String $desc) : void{
+        $group = new stdClass();
+        $group->name = $groupeName;
+        $group->description = $desc;
+        return groups_create_group($group);
         }
 
     public function deleteGroups(String $groupeName): void{
@@ -17,37 +15,39 @@ class GroupsDB {
         $DB->delete_records('groups', array('name' => $groupeName));
     }
 
-    public function updateGroups(String $groupeName, String $desc): void{
+    public function updateGroups(String $oldGroupName, String $groupeName, String $desc): void{
         global $DB;
-        $groupdata = $DB->get_record('groups', array('name' => $groupeName));
-        $groupdata->description = $desc;
-        $DB->update_record('groups', $groupdata);
+        $group = groups_get_group_by_name($oldGroupName);
+        $group->name = $groupeName;
+        $group->description = $desc;
+        return groups_update_group($group);
+
     }
 
     public function addMember(String $groupeName, String $username): void{
         global $DB;
-        $groupdata = $DB->get_record('groups', array('name' => $groupeName));
-        $userdata = $DB->get_record('user', array('username' => $username));
-        groups_add_member($groupdata, $userdata);
+        $group = $DB->get_record('groups', array('name' => $groupeName));
+        $user = $DB->get_record('user', array('username' => $username));
+        groups_add_member($group, $user);
     }
 
     public function deleteMember(String $groupeName, String $username): void{
         global $DB;
-        $groupdata = $DB->get_record('groups', array('name' => $groupeName));
-        $userdata = $DB->get_record('user', array('username' => $username));
-        groups_remove_member($groupdata, $userdata);
+        $group = $DB->get_record('groups', array('name' => $groupeName));
+        $user = $DB->get_record('user', array('username' => $username));
+        groups_remove_member($group, $user);
     }
 
     public function getGroups(String $groupeName): String{
         global $DB;
-        $groupdata = $DB->get_record('groups', array('name' => $groupeName));
-        return $groupdata;
+        $group = $DB->get_record('groups', array('name' => $groupeName));
+        return $group;
     }
 
     public function getMembers(String $groupeName): String{
         global $DB;
-        $groupdata = $DB->get_record('groups', array('name' => $groupeName));
-        $members = groups_get_members($groupdata->id);
+        $group = $DB->get_record('groups', array('name' => $groupeName));
+        $members = groups_get_members($group->id);
         return $members;
     }
 }
