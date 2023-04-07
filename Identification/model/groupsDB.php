64542cbc38be1b2cf468873a19f6cccc63dd7c35
Michaël Besily <michael.besily@gmail.com>
2023-04-07 13:51:50 +0000

    Update groupsDB.php

--- Identification/model/groupsDB.php
+++ Identification/model/groupsDB.php
@@ -3,63 +3,14 @@
 class GroupsDB {
 
 
+    
     public function addGroups(String $groupeName, String $desc) : void{
-        global $DB, $USER, $CFG;
-
-        // Récupérer l'ID du dernier cours créé
-       // $lastcourse = $DB->get_record_sql('SELECT id FROM {course} ORDER BY id DESC LIMIT 1');
-        // $lastcourseid = ($lastcourse) ? $lastcourse->id : 0;
-        //$newcourseid = $lastcourseid + 1;
-
-        // Créer le contexte pour le cours
-        $context = context_course::instance($newcourseid);
-
-        // Définir les données du groupe
-        $data = new stdClass();
-        $data->courseid = $newcourseid;
-        $data->name = trim($groupeName);
-        $data->description = trim($desc);
-        $data->timecreated = time();
-        $data->timemodified = $data->timecreated;
-        $data->visibility = GROUPS_VISIBILITY_ALL;
-
-        // Insérer le nouveau groupe
-        $data->id = $DB->insert_record('groups', $data);
-
-        // Récupérer les données du groupe créé
-        $group = $DB->get_record('groups', array('id'=>$data->id));
-
-
-        // Mettre à jour les caches de groupes pour le cours
-        cache_helper::invalidate_by_definition('core', 'groupdata', array(), array($newcourseid));
-        \core_group\visibility::update_hiddengroups_cache($newcourseid);
-
-        // Mettre à jour la conversation de groupe (si applicable)
-        if (\core_message\api::can_create_group_conversation($USER->id, $context)) {
-            if (!empty($data->enablemessaging)) {
-                \core_message\api::create_conversation(
-                    \core_message\api::MESSAGE_CONVERSATION_TYPE_GROUP,
-                    [],
-                    $group->name,
-                    \core_message\api::MESSAGE_CONVERSATION_ENABLED,
-                    'core_group',
-                    'groups',
-                    $group->id,
-                    $context->id
-                );
-            }
+        $group = new stdClass();
+        $group->name = $groupeName;
+        $group->description = $desc;
+        groups_create_group($group);
         }
-
-        // Déclencher l'événement de création de groupe
-        $params = array(
-            'context' => $context,
-            'objectid' => $group->id
-        );
-        $event = \core\event\group_created::create($params);
-        $event->add_record_snapshot('groups', $group);
-        $event->trigger();
-        }
-
+    
     public function deleteGroups(String $groupeName): void{
         global $DB;
         $DB->delete_records('groups', array('name' => $groupeName));
