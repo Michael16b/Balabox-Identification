376613fc6434b278eaf49984c318523a64935db2
Michaël Besily <michael.besily@gmail.com>
2023-04-07 13:13:25 +0000

    add groups lib

--- Identification/index2.php
+++ Identification/index2.php
@@ -19,6 +19,7 @@ require (__ROOT__.'/model/groupsDB.php');
 require (__ROOT__.'/model/tokenGenerator.php');
 require (__ROOT__.'/model/usersConnected.php');
 require_once(__ROOT__.'/user/lib.php');
+require_once(__ROOT__.'/group/lib.php');
 require_once (__ROOT__.'/config2.php');
 require_once (__ROOT__.'/config.php');
 // ApplicationController
