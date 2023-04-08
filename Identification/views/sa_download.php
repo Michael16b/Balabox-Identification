4300884fe7823553fd08cab9a4af8951d9940386
Michaël <michael.besily@gmail.com>
2023-04-08 15:32:04 +0200

    delete nav bar

--- Identification/views/sa_download.php
+++ Identification/views/sa_download.php
@@ -3,16 +3,6 @@ include __ROOT__."/views/header.html";
 ?>
 
 <body>
-    <!-- HEADER -->
-    <nav class="navbar border-bottom">
-        <div class="container-fluid">
-            <img src="../static/img/logo_balabox.png" alt="Logo"  height="30" class="d-inline-block align-text-top">
-            <form class="justify-content-start ">
-                <a href="sa_classCreate"><button class="btn btn-outline-success me-2" type="button">Créer une classe</button></a>
-                <a href="sa_userCreate"><button class="btn btn-sm btn-outline-secondary" type="button">Créer un utilisateur</button></a>
-            </form>
-        </div>
-    </nav>
     
     <!-- BODY -->
     <div class="container my-4">
