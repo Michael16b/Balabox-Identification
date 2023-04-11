bf8f6ee6f15a78ee1c3d8e84b18e5e044efd0bad
Thomas-Poulain <e2202656@etud.univ-ubs.fr>
2023-04-11 11:33:08 +0200

    test de la connection

--- Identification/controllers/connect.php
+++ Identification/controllers/connect.php
@@ -36,7 +36,7 @@ class ConnectController extends Controller{
     public function post($request){
         try{
 			// test //
-			
+			/*
 			$role = 1;
 			$_SESSION['username'] = $request['username'];
 			$_SESSION['password'] = $request['password'];
@@ -45,10 +45,10 @@ class ConnectController extends Controller{
 			$uc = new UsersConnected();
 			$uc->newConnection($request['username'], $role);
 			$this-> render('/sa_classCreate',[]);
-			
+			*/
 			//fin test //
 			//recuperation de l'utilisateur
-			/*
+
 			$userdb = new UserDB();
 			$user = $userdb->getRecord($request['username']);
 			$username = $request['username'];
@@ -79,7 +79,7 @@ class ConnectController extends Controller{
 			else{
 				$this-> render('/main',[]);
 				echo "utilisateur introuvable";
-			}		*/				
+			}			
         }catch (Error $e){
             echo $e;
 	    $this-> render('/error',['surname' => 'Error', 'password' => 'Error', 'idprof' => null]);
