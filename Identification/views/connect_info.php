310925f01d5c687238c3d59a0c9939d768f217ce
Michaël <michael.besily@gmail.com>
2023-04-13 00:37:40 +0200

    add new UI for connect_info

--- Identification/views/connect_info.php
+++ Identification/views/connect_info.php
@@ -1,15 +1,15 @@
 <?php
-
 include __ROOT__."/views/header.html";
-
-echo "role =".$_SESSION['role'];
-echo "<br>";
-echo "Numéro téléphone élève =". $_SESSION['username'];
-echo "<br>";
-echo "mdp =". $_SESSION['password'];
 ?>
+
 <body>
-    <form form action="/disconnect" method="post">
-        <button type="submit"> se déconnecter</button>
-    </form>
+<div class="container">
+        <h1>Informations utilisateur</h1>
+        <p><i class="fa fa-user"></i> Rôle : <?php echo $_SESSION['role']; ?></p>
+        <p><i class="fa fa-phone"></i> Numéro de téléphone élève : <?php echo $_SESSION['username']; ?></p>
+        <p><i class="fa fa-lock"></i> Mot de passe : <?php echo $_SESSION['password']; ?></p>
+        <form action="/disconnect" method="post">
+            <button type="submit" class="btn btn-danger"><i class="fa fa-sign-out"></i> Se déconnecter</button>
+        </form>
+    </div>
 </body>
