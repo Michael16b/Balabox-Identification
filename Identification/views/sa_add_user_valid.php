58ba6286dadc2043bde7f319a7d2a4e9399fdc8c
Michaël <michael.besily@gmail.com>
2023-04-13 01:01:26 +0200

    add new UI sa_add_user

--- Identification/views/sa_add_user_valid.php
+++ Identification/views/sa_add_user_valid.php
@@ -4,41 +4,19 @@ include __ROOT__."/views/header.html";
 
 <body>
     <div class="container">
+        <h1>Informations utilisateur</h1>
         <div class="row">
-            <div class="col-md-12">
-                <h1>Informations utilisateur</h1>
-                <div class="row">
-                    <div class="col-md-3">
-                        <p><i class="fas fa-user"></i> Nom d'utilisateur :</p>
-                    </div>
-                    <div class="col-md-9">
-                        <p><?php echo $data[0]; ?></p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        <p><i class="fas fa-lock"></i> Mot de passe :</p>
-                    </div>
-                    <div class="col-md-9">
-                        <p><?php echo $data[1]; ?></p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        <p><i class="fas fa-user-tag"></i> Rôle :</p>
-                    </div>
-                    <div class="col-md-9">
-                        <p><?php echo $data[2]; ?></p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-3">
-                        <p><i class="fas fa-graduation-cap"></i> Classe :</p>
-                    </div>
-                    <div class="col-md-9">
-                        <p><?php echo $data[3]; ?></p>
-                    </div>
-                </div>
+            <div class="col-md-3">
+                <p><i class="fas fa-user"></i> Nom d'utilisateur :</p>
+                <p><i class="fas fa-lock"></i> Mot de passe :</p>
+                <p><i class="fas fa-user-tag"></i> Rôle :</p>
+                <p><i class="fas fa-graduation-cap"></i> Classe :</p>
+            </div>
+            <div class="col-md-9">
+                <p><?php echo $data[0]; ?></p>
+                <p><?php echo $data[1]; ?></p>
+                <p><?php echo $data[2]; ?></p>
+                <p><?php echo $data[3]; ?></p>
             </div>
         </div>
     </div>
