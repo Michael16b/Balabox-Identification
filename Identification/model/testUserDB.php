0e25ed0bd504f85b58e2995e541ce0658f53eb75
Michaël <michael.besily@gmail.com>
2023-04-12 21:42:43 +0200

    add TestUserDB

--- Identification/model/testUserDB.php
+++ Identification/model/testUserDB.php
@@ -1,25 +1,123 @@
 <?php
 
-// inclure la classe UserDB à tester
-require_once('userDB.php');
+require_once(dirname(__FILE__) . '/userDB.php');
 
-$validTest = 0;
 
-echo "Test de la méthode getRecord \n";
+class TestUserDB {
+    
 
-$username = 'testuser';
+    public function TestGetRecord(string $username){
+        $userDB = new UserDB();
+        $user = $userDB->getRecord($username);
+        if ($user != false){
+            echo "TestGetRecord: OK";
+        } else {
+            echo "TestGetRecord: KO";
+        }
 
-echo "Cas 1: tester la méthode getRecord avec un nom d'utilisateur inexistant : $username  \n";
-$userDB = new UserDB();
-
-try {
-    $record = $userDB->getRecord('testuser');
-    if ($record !== null) {
-        echo "La méthode getRecord ne fonctionne pas correctement. \n";
     }
-} catch (Exception $e) {
-    echo "La méthode getRecord fonctionne pas correctement et renvoie l'exception : $e. \n";
+
+    public function TestGetUserById(int $id){
+        $userDB = new UserDB();
+        $user = $userDB->getUserById($id);
+        if ($user != false){
+            echo "TestGetUserById: OK";
+        } else {
+            echo "TestGetUserById: KO";
+        }
+
+    }
+
+    public function TestGetUsers(){
+        $userDB = new UserDB();
+        $users = $userDB->getUsers();
+        if ($users != false){
+            echo "TestGetUsers: OK";
+        } else {
+            echo "TestGetUsers: KO";
+        }
+
+    }
+
+
+    public function TestCheckUserName(String $username) {
+        $userDB = new UserDB();
+        $user = $userDB->checkUserName($username);
+        if ($user != false){
+            echo "TestCheckUserName: OK";
+        } else {
+            echo "TestCheckUserName: KO";
+        }
+
+        
+    }
+
+    public function TestAddUser(String $firstName, String $lastName, int $role = 4){
+        $userDB = new UserDB();
+        $user = $userDB->addUser($firstName, $lastName, $role);
+        if ($user != false){
+            echo "TestAddUser: OK";
+        } else {
+            echo "TestAddUser: KO";
+            }
+        }
+
+    public function TestAddRolesSystemMembers(String $username, String $role): void{
+        global $DB;
+        $userDB = new UserDB();
+        $userDB->addRolesSystemMembers($username, $role);
+        $user = $userDB->getRecord($username);
+        $roles =$DB->get_record('role_assignments', array('userid' => $user->id));
+        if ($roles->roleid == $role){
+            echo "TestAddRolesSystemMembers: OK";
+        } else {
+            echo "TestAddRolesSystemMembers: KO";
+        }
+    }
+    
+    public function TestGetUser_role(string $username) {
+        $userDB = new UserDB();
+        $role = $userDB->getUser_role($username);
+        if ($role != false){
+            echo "TestGetUser_role: OK";
+        } else {
+            echo "TestGetUser_role: KO";
+        }
+    }
+
+    public function TestGetGroupOfUser(String $username) {
+        $userDB = new UserDB();
+        $group = $userDB->getGroupOfUser($username);
+        if ($group != false){
+            echo "TestGetGroupOfUser: OK";
+        } else {
+            echo "TestGetGroupOfUser: KO";
+        }
+    }
+
+
+    public function TestDeleteUser(String $username): void{
+        $userDB = new UserDB();
+        $userDB->deleteUser($username);
+        $user = $userDB->getRecord($username);
+        if ($user == false){
+            echo "TestDeleteUser: OK";
+        } else {
+            echo "TestDeleteUser: KO";
+        }
+    }
+
+    public function TestUpdateUser(String $username, String $firstName, String $lastName, bool $password) {
+        $userDB = new UserDB();
+        $userDB->updateUser($username, $firstName, $lastName, $password);
+        $user = $userDB->getRecord($username);
+        if ($user->firstname == $firstName && $user->lastname == $lastName){
+            echo "TestUpdateUser: OK";
+        } else {
+            echo "TestUpdateUser: KO";
+        }
+    }
+
+
 }
-
-
 ?>
