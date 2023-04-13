1fa7e4e7aeea66cc138cf27531ac88f04791ea6d
Thomas-Poulain <e2202656@etud.univ-ubs.fr>
2023-04-13 11:17:02 +0200

    test cors

--- Identification/controllers/groups.php
+++ Identification/controllers/groups.php
@@ -5,12 +5,19 @@ class GroupesController extends Controller{
     public function get($request){
         try{
             header("Access-Control-Allow-Origin: *");
+            header('Access-Control-Allow-Methods: GET, POST');
+            header("Access-Control-Allow-Headers: X-Requested-With");
+            header("Access-Control-Allow-Credentials: true");
             header("HTTP/1.1 200 OK");
             $groups = new GroupsDB();
             echo json_encode($groups->getGroups());
         }catch(Exception $e){
             header("Access-Control-Allow-Origin: *");
+            header('Access-Control-Allow-Methods: GET, POST');
+            header("Access-Control-Allow-Headers: X-Requested-With");
+            header("Access-Control-Allow-Credentials: true");
             header("HTTP/1.1 400 Bad Request");
+
             echo json_encode(array('error' => $e->getMessage()));
         }
     }
